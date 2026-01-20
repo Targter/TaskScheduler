28830cba9cdef006fbb74cb89d8ccee69211c288
Abhay Bansal <targeter001@gmail.com>
2026-01-20 17:47:51 +0530

    adding initial frontend

---
+++ frontend2/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+const page = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
+    </div>
+  );
+};
+export default page;
