84d7eead96f4f5d1876b02059d0cccb9a1b9c887
Abhay Bansal <targeter001@gmail.com>
2026-02-02 02:26:42 +0530

    Refactor : schema + validatoin

--- my-app/app/api/oauth/instagram/route.ts
+++ my-app/app/api/oauth/instagram/route.ts
@@ -8,9 +8,31 @@ export async function GET() {
   const redirectUri = `${process.env.NEXTAUTH_URL}/api/oauth/instagram/callback`;
   
   // Permissions needed for scheduling
-  const scope = "instagram_basic,instagram_content_publish,pages_show_list,pages_read_engagement"; 
+  // const scope = "instagram_basic,instagram_content_publish,pages_show_list,pages_read_engagement"; 
 
-  const url = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+   const scope = [
+    "pages_show_list",
+    "pages_read_engagement",
+    "instagram_basic",
+    "instagram_content_publish",
+  ].join(" ");
+
+  const params = new URLSearchParams({
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    scope,
+    response_type: "code",
+    state: crypto.randomUUID(), // 🔐 important
+  });
+
+  const url = `https://www.facebook.com/v18.0/dialog/oauth?${params.toString()}`;
+
+  console.log("Facebook OAuth URL:", url);
+
+
+
+  // const url = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+  // console.log("url:",url)
 
   return redirect(url);
 }
