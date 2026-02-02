e17f336317a2d7a4b3457820b2cfeb74870d8229
Abhay Bansal <targeter001@gmail.com>
2026-02-02 11:46:31 +0530

    Refactor : Meta Error

--- my-app/app/api/oauth/instagram/route.ts
+++ my-app/app/api/oauth/instagram/route.ts
@@ -8,8 +8,8 @@ export async function GET() {
   const redirectUri = `${process.env.NEXTAUTH_URL}/api/oauth/instagram/callback`;
   
   // Permissions needed for scheduling
-  const scope = "instagram_basic,instagram_content_publish,pages_show_list,pages_read_engagement"; 
-
+  const scope = "pages_show_list,pages_read_engagement"; 
+// instagram_basic,instagram_content_publish,
   const url = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
 
   return redirect(url);
